989cc9ed263796c61bdf937dbaba90c973746b86
Simon Turquier <turquiersimon@hotmail.fr>
2018-11-04 23:03:27 +0100

    Fix assignable type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { bootstrap } from 'vesper'
+import { Category } from './entity/Category'
+import { Ingredient } from './entity/Ingredient'
+import { Preparation } from './entity/Preparation'
+import { Recipe } from './entity/Recipe'
 
 bootstrap({
   port: 8080,
   controllers: [__dirname + '/controller/**/*.ts'],
-  entities: [__dirname + '/entity/**/*.ts'],
+  entities: [Category, Ingredient, Preparation, Recipe],
   resolvers: [__dirname + '/resolver/**/*.ts'],
   schemas: [__dirname + '/schema/**/*.graphql']
 }).then(() => {
