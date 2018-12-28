83759aa363566993786e186a6dfbc3fdafcfaa7a
Simon Turquier <turquiersimon@hotmail.fr>
2018-12-28 18:37:48 +0100

    Remove ClearDB config

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   "type": "mysql",
-  "host": process.env.CLEARDB_DATABASE_HOST || "localhost",
+  "host": "localhost",
   "port": 3306,
-  "username": process.env.CLEARDB_DATABASE_USERNAME || "root",
-  "password": process.env.CLEARDB_DATABASE_PASSWORD || "",
-  "database": process.env.CLEARDB_DATABASE_NAME || "cookbook_db",
+  "username": "root",
+  "password": "",
+  "database": "cookbook_db",
   "synchronize": true
 }
