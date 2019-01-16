aa0abe653dbb3462c60b7bce5e42634a6c0f9e06
Simon Turquier <turquiersimon@hotmail.fr>
2019-01-16 11:34:46 +0100

    Fix dotenv variables

---
+++ config.env.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+
+const config = {
+  development: {
+    ormConfig: {
+      type: 'mysql',
+      host: process.env.LOCAL_DATABASE_URI,
+      port: 3306,
+      username: process.env.LOCAL_DATABASE_USERNAME,
+      password: process.env.LOCAL_DATABASE_PASSWORD,
+      database: process.env.LOCAL_DATABASE_NAME,
+      synchronize: true,
+      cache: true,
+      entities: [__dirname + '/src/entities/*.ts']
+    },
+  },
+  production: {
+    ormConfig: {
+      type: 'mysql',
+      host: process.env.AWS_RDS_DATABASE_URI,
+      port: 3306,
+      username: process.env.AWS_RDS_DATABASE_USERNAME,
+      password: process.env.AWS_RDS_DATABASE_PASSWORD,
+      database: process.env.AWS_RDS_DATABASE_NAME,
+      synchronize: false,
+      cache: true,
+      entities: [__dirname + '/build/entities/*.js']
+    },
+  },
+}
+
+const envConfig = config[process.env.NODE_ENV || 'development']
+export default envConfig
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,17 @@ import * as TypeGraphQL from 'type-graphql'
 import * as TypeORM from 'typeorm'
 import { Container } from 'typedi'
 import { CategoryResolver, IngredientResolver, RecipeResolver } from './resolvers'
+import envConfig from '../config.env'
 
 TypeGraphQL.useContainer(Container)
 TypeORM.useContainer(Container)
 
 const port = parseInt(process.env.PORT) || 5000
+const { ormConfig } = envConfig
 
 async function bootstrap(): Promise<void> {
   try {
-    await TypeORM.createConnection()
+    await TypeORM.createConnection(ormConfig)
 
     const schema = await TypeGraphQL.buildSchema({
       resolvers: [CategoryResolver, IngredientResolver, RecipeResolver],
