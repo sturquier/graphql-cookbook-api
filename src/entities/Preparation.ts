7c6d26959c2bf9ac90f8e3fc1ba6ea5c96831549
Simon Turquier <turquiersimon@hotmail.fr>
2019-01-18 15:38:35 +0100

    Fix preparation delete cascade

--- src/entities/Preparation.ts
+++ src/entities/Preparation.ts
@@ -19,6 +19,6 @@ export class Preparation {
   instruction: string
 
   @Field(() => Recipe)
-  @ManyToOne(() => Recipe, recipe => recipe.preparation)
+  @ManyToOne(() => Recipe, recipe => recipe.preparation, { onDelete: 'CASCADE' })
   recipe: Promise<Recipe>
 }
